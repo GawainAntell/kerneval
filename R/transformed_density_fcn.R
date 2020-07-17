091069fdd18f0d2e19567db7589888b2fe75eaa0
Gwen Antell <gwenantell@gmail.com>
2020-07-17 12:50:37 +0100

    Fix bug in density rescaling so KDE integrates to unity

--- R/transformed_density_fcn.R
+++ R/transformed_density_fcn.R
@@ -29,7 +29,8 @@
 #' @param b The upper limit for density estimation,
 #' on the original, untransformed scale. Default is \code{max(x)}.
 #' @param ... Further arguments passed on to \code{\link[stats]{density}}.
-#' @return An S3 density.
+#' @return An S3 density, with the additional attribute \code{$xTrans},
+#' which is the location of \code{$x} values along the transformed axis.
 #' @export
 #' @references
 #' \insertRef{Barmi00}{kerneval}
@@ -37,14 +38,9 @@
 # Barmi & Simonoff 2000 method
 # One should NOT provide the arguments to/from or lower/upper.
 # Add tweak to uniroot function within inverse, for badly behaved data?
-transdens <- function(x, w, reflect = FALSE, a=NULL, b=NULL, ...){
+transdens <- function(x, w, reflect = FALSE, a = NULL, b = NULL, ...){
   if (is.null(a)){
-    test <- c(is.infinite(w(0)), is.na(w(0)))
-    if (any(test)){
-      a <- min(x)
-    } else {
-      a <- 0
-    }
+    a <- min(x)
   }
 
   # transform the observations
@@ -53,8 +49,8 @@ transdens <- function(x, w, reflect = FALSE, a=NULL, b=NULL, ...){
   }
   Y <- sapply(x, cdf)
 
-  # define the boundaries of estimation on the transformed scale
-  aYscale <- cdf(a)
+  # Define upper boundary of estimation on the transformed scale
+  # Lower boundary will always be 0 on transformed scale (CDF from a to a)
   if (is.null(b)){
     b <- max(x)
     bYscale <- max(Y)
@@ -64,28 +60,20 @@ transdens <- function(x, w, reflect = FALSE, a=NULL, b=NULL, ...){
 
   # estimate kernel density, with boundary reflection if specified
   if (reflect){
-    kde <- density.reflected(Y, lower = aYscale, upper = bYscale, ...)
+    kde <- density.reflected(Y, lower = 0, upper = bYscale, ...)
   } else {
-    kde <- stats::density(Y, from = aYscale, to = bYscale, ...)
+    kde <- stats::density(Y, from = 0, to = bYscale, ...)
   }
-
-  # scale to integrate to one
-  n <- length(x)
-  muHat <- n * sum( w(x)^-1 )^-1
-  kde$y <- muHat * kde$y
 
   # Back-transform from the y=W(x) argument to x.
   inv <- GoFKernel::inverse(cdf, lower = a, upper = b)
-
-  kde$xTrans <- kde$x
+  kde$xTrans <- kde$x # save the estimate on the y-scale too
   kde$x <- sapply(kde$x, inv)
 
-  # if w(x) is undefined at 0, then min and max of X can be off
-  # (albeit only slightly) from values at which kernel can estimate
-#  f <- stats::approxfun(kde$x, kde$y)
-#  lwr <- min(kde$x)
-#  upr <- max(kde$x)
-#  append(list(f=f, lower=lwr, upper=upr), kde)
+  # scale to integrate to one (i.e. multiply by mu-hat)
+  kdeFun <- approxfun(kde$x, kde$y)
+  muHat <- 1/integral(kdeFun, min(kde$x), max(kde$x))
+  kde$y <- muHat * kde$y
 
   return(kde)
 }
