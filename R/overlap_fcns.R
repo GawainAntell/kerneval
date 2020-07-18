62e94b3057792a375126f9ff217c683c4a65411b
Gwen Antell <gwenantell@gmail.com>
2020-07-18 17:18:57 +0100

    New function: crop and rescale a PDF over a given interval. Remove extrap option from overlap functions.

--- R/overlap_fcns.R
+++ R/overlap_fcns.R
@@ -1,61 +1,94 @@
 
+#' Rescale a PDF over an interval
+#'
+#' @param pdf A univariate probability density distribution.
+#' @param a,b The new lower and upper limits of the distribution.
+#' The density will be rescaled such that the integral over the
+#' interval `[a,b]` sums to unity.
+#' @return An S3 density object.
+
+dscaler <- function(pdf, a, b){
+  x <- pdf$x
+  if (a < min(x) | b > max(x)){
+    stop('limits outside the range of available data')
+  }
+  fhat <- stats::approxfun(pdf$x, pdf$y)
+
+  # truncate to the new interval
+  inRange <- x >= a & x <= b
+  pdf$x <- pdf$x[inRange]
+  pdf$y <- pdf$y[inRange]
+
+  # Explicitly add end points at exact a and b values
+  # Otherwise PDF will be truncated just shy of the given limits
+  # Skip this if x values at a and b exist already
+  newX <- pdf$x
+  if (min(newX) != a){
+    pdf$x <- c(a, pdf$x)
+    pdf$y <- c(fhat(a), pdf$y)
+  }
+  if (max(newX) != b){
+    pdf$x <- c(pdf$x, b)
+    pdf$y <- c(pdf$y, fhat(b))
+  }
+
+  # rescale the density over the new interval
+  mu <- 1 / pracma::integral(fhat, a, b)
+  pdf$y <- pdf$y * mu
+  return(pdf)
+}
+
+
 #' Calculate Hellinger distance
 #'
 #' The Hellinger distance (H) between two probability measures ranges from 0
 #' (identical distributions) to 1 (non-overlapping distributions).
 #'
+#' Let `f(x)` and `g(x)` be the probability density functions for comparison.
+#' The squared Hellinger distance is
+#' \eqn{0.5 \int(\sqrt f(x) - \sqrt g(x) ^2) dx}. Conveniently for numeric
+#' integration, the expression can be written as the integral of a product
+#' instead of a difference: \eqn{1 - \int \sqrt{f(x)g(x)} dx}. H is related
+#' to the Bhattacharyya coefficient BC: \eqn{H = \sqrt{1 - BC}}. Elsewhere,
+#' Hellinger distances are sometimes reported as the square (\eqn{H^2})
+#' [@Warren08; DiCola17], or are not rescaled and so range
+#' from 0 to \sqrt(2) [@Nikulin01].
+#'
 #' @param d1,d2 A density distribution.
-#' @param extrap Logical. If axes limits of \code{d1} and \code{d2} differ,
-#' should they be extrapolated to match? The density function
-#' will estimate 0 probability in the region of extrapolation.
+#' @param a,b The lower and upper limits of comparison.
+#' Each density will be rescaled such that the integral over the
+#' interval `[a,b]` sums to unity. If empty, the minimum and maximum
+#' of the intersection of `d1$x` and `d2$x` will be used.
 #' @return A numeric value between 0 and 1 inclusive.
 #' @export
 
-# Hellinger's H
-hell <- function (d1, d2, extrap = TRUE) {
-  if (extrap==TRUE){
-    a <- min(d1$x, d2$x)
-    b <- max(d1$x, d2$x)
-
-    # ensure the density estimate extends along the full x-axis
-    if (min(d1$x) > a){
-      d1$x <- c(a, d1$x)
-      d1$y <- c(0, d1$y)
-    }
-    if (min(d2$x) > a){
-      d2$x <- c(a, d2$x)
-      d2$y <- c(0, d2$y)
-    }
-    if (max(d1$x) < b){
-      d1$x <- c(d1$x, b)
-      d1$y <- c(d1$y, 0)
-    }
-    if (max(d2$x) < b){
-      d2$x <- c(d2$x, b)
-      d2$y <- c(d2$y, 0)
-    }
-  } else {
-    if (min(d1$x) > max(d2$x) | max(d1$x) < min(d2$x)){
-      stop('no overlap in ranges over which densities are defined')
-    }
+hell <- function (d1, d2, a = NULL, b = NULL) {
+  if (min(d1$x) > max(d2$x) | max(d1$x) < min(d2$x)){
+    stop('no overlap in ranges over which densities are defined')
   }
 
-  fun1 <- stats::approxfun(d1$x, d1$y)
-  fun2 <- stats::approxfun(d2$x, d2$y)
-  intgrnd <- function(x) sqrt(fun1(x) * fun2(x))
+  # standardise PDFs over the same x-interval
+  if (is.null(a)){
+    a <- max(min(d1$x), min(d2$x))
+  }
+  if (is.null(b)){
+    b <- min(max(d1$x), max(d2$x))
+  }
+  d1scl <- dscaler(d1, a, b)
+  d2scl <- dscaler(d2, a, b)
 
-  # re-define a and b after extrapolation
-  # or define in the first place if no extrapolation
-  a <- max(min(d1$x), min(d2$x))
-  b <- min(max(d1$x), max(d2$x))
+  fun1 <- stats::approxfun(d1scl$x, d1scl$y)
+  fun2 <- stats::approxfun(d2scl$x, d2scl$y)
+  intgrnd <- function(x) sqrt(fun1(x) * fun2(x))
   int <- pracma::integral(intgrnd, a, b)
   if (int > 1){
     int <- 1
-#    warning('estimated Bhattacharyya distance greater than 1')
+    # warning('estimated Bhattacharyya distance greater than 1')
   }
   h <- sqrt(1 - int)
   h
 }
+
 
 #' Calculate Schoener's D metric of niche overlap
 #'
@@ -77,46 +110,26 @@ hell <- function (d1, d2, extrap = TRUE) {
 #' @references
 #' \insertRef{Schoener68}{kerneval}
 
-# Schoener's D, continuous version
-schoenr <- function (d1, d2, extrap = TRUE) {
-
-  if (extrap==TRUE){
-    a <- min(d1$x, d2$x)
-    b <- max(d1$x, d2$x)
-
-    # ensure the density estimate extends along the full x-axis
-    if (min(d1$x) > a){
-      d1$x <- c(a, d1$x)
-      d1$y <- c(0, d1$y)
-    }
-    if (min(d2$x) > a){
-      d2$x <- c(a, d2$x)
-      d2$y <- c(0, d2$y)
-    }
-    if (max(d1$x) < b){
-      d1$x <- c(d1$x, b)
-      d1$y <- c(d1$y, 0)
-    }
-    if (max(d2$x) < b){
-      d2$x <- c(d2$x, b)
-      d2$y <- c(d2$y, 0)
-    }
-  } else {
-    if (min(d1$x) > max(d2$x) | max(d1$x) < min(d2$x)){
-      stop('no overlap in ranges over which densities are defined')
-    }
+schoenr <- function (d1, d2, a = NULL, b = NULL) {
+  if (min(d1$x) > max(d2$x) | max(d1$x) < min(d2$x)){
+    stop('no overlap in ranges over which densities are defined')
   }
 
-  fun1 <- stats::approxfun(d1$x, d1$y)
-  fun2 <- stats::approxfun(d2$x, d2$y)
+  # standardise PDFs over the same x-interval
+  if (is.null(a)){
+    a <- max(min(d1$x), min(d2$x))
+  }
+  if (is.null(b)){
+    b <- min(max(d1$x), max(d2$x))
+  }
+  d1scl <- dscaler(d1, a, b)
+  d2scl <- dscaler(d2, a, b)
+
+  fun1 <- stats::approxfun(d1scl$x, d1scl$y)
+  fun2 <- stats::approxfun(d2scl$x, d2scl$y)
   difFun <- function(x){
     abs(fun1(x) - fun2(x))
   }
-
-  # re-define a and b after extrapolation
-  # or define in the first place if no extrapolation
-  a <- max(min(d1$x), min(d2$x))
-  b <- min(max(d1$x), max(d2$x))
   int <- pracma::integral(difFun, a, b)
   if (int > 2){
     int <- 2
