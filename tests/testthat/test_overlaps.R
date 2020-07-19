91f1dfc52ff673c74e958bf7d08283bdead7c84d
Gwen Antell <gwenantell@gmail.com>
2020-07-19 16:12:34 +0100

    Simplify test data. Add tests to generate errors.

--- tests/testthat/test_overlaps.R
+++ tests/testthat/test_overlaps.R
@@ -1,54 +1,17 @@
 context('Overlap values')
 library(kerneval)
 
-normPdf <- function(x, shft){
-  stats::dnorm(x, (25 + shft), sd=9.7)
-}
-normSim <- function(n, shft){
-  stats::rnorm(n, (25 + shft), sd=9.7)
-}
-# With a mean=25, sd=9.7, 99% of normal distribution mass falls from 0-50
-# p <- 0.99 + pnorm(0, mean=25, sd=9.7)
-# qnorm(p, mean=25, sd=9.7)
-
-xmn <- 0
-xmx <- 100
-n <- 100
-shft <- 25
-
-# equivalency metric (1 minus Hellinger's H)
-intgrnd <- function(x) sqrt(normPdf(x, 0) * normPdf(x, shft))
-int <- pracma::integral(intgrnd, xmn, xmx)
-# for norm vs norm, shft=10, H is .024
-# versus 0.35 if integrated over -Inf to Inf
-H <- sqrt(1 - int)
-
-# Schoener's D
-difFun <- function(x) abs(normPdf(x, 0) - normPdf(x, shft))
-int2 <- pracma::integral(difFun, xmn, xmx)
-# for norm vs norm, shft=10, I is 0.69
-# versus 0.61 if integrated over -Inf to Inf
-D <- 1 - int2 * 0.5
-
-x1 <- normSim(n, 0)
-x2 <- normSim(n, shft)
-x1 <- x1[x1 >= xmn & x1 <= xmx]
-x2 <- x2[x2 >= xmn & x2 <= xmx]
-
-# build density on x1 and x2
+n <- 500
+x1 <- stats::rnorm(n, 0, 2)
+x2 <- stats::rnorm(n, 2, 3)
 d1 <- density(x1)
 d2 <- density(x2)
 
 # tests -------------------------------------------------------------------
 
-test_that('identical dists return max overlap', {
+test_that('identical PDFs give maximum overlap', {
   expect_equivalent(hell(d1, d1), 0)
   expect_equivalent(schoenr(d1, d1), 1)
-})
-
-test_that('estimated overlap is within tolerance of true', {
-  expect_equal(hell(d1, d2), H,    tolerance = 0.2)
-  expect_equal(schoenr(d1, d2), D, tolerance = 0.2)
 })
 
 test_that('function is commutative', {
@@ -58,18 +21,31 @@ test_that('function is commutative', {
                     schoenr(d2, d1))
 })
 
-xFar <- x1 + 2 * max(x1)
-d3 <- density(xFar)
-
-test_that('non-overlapping dists return min overlap', {
-  expect_equal(hell(d1, d3, extrap = TRUE), 1,
-               tolerance = 0.1)
-  expect_equal(schoenr(d1, d3, extrap = TRUE), 0,
-               tolerance = 0.1)
+test_that('bad limits give error', {
+  expect_error(
+    hell(d1, d2, a = 5, b = 0),
+    'upper limit must be greater than lower limit'
+  )
+  expect_error(
+    schoenr(d1, d2, a = 5, b = 0),
+    'upper limit must be greater than lower limit'
+  )
+  expect_error(
+    hell(d1, d2, a = -100, b = 5),
+    'limits outside the range of available data'
+  )
+  expect_error(
+    hell(d1, d2, a = -100, b = 5),
+    'limits outside the range of available data'
+  )
 })
 
-test_that('extrapolation works correctly', {
-  expect_error(hell(d1, d3, extrap = FALSE),
-               'no overlap in ranges over which densities are defined')
-})
+test_that('limits arguments are used', {
+  h1 <- hell(d1, d2, a = -5, b = 5)
+  h2 <- hell(d1, d2, a = 0,  b = 5)
+  expect_false(identical(h1, h2))
 
+  D1 <- schoenr(d1, d2, a = -5, b = 5)
+  D2 <- schoenr(d1, d2, a = 0,  b = 5)
+  expect_false(identical(D1, D2))
+})
